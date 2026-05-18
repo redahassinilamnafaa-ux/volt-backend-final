790f65f64e5f804a8a0c0057d403ea0ccf7e31fa
redahassinilamnafaa-ux <reda.hassinilamnafaa@gmail.com>
2026-05-18 20:17:57 +0200

    Delete api/config.js

--- api/config.js
+++
@@ -1,9 +0,0 @@
-const cors = require("../lib/cors");
-
-module.exports = function handler(req, res) {
-  cors(res);
-  if (req.method === "OPTIONS") return res.status(200).end();
-  res.json({
-    stripe_key: process.env.STRIPE_PUBLISHABLE_KEY || ""
-  });
-};
