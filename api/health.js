5ab479fc43e820163a3341067ae126b873dfe7e2
redahassinilamnafaa-ux <reda.hassinilamnafaa@gmail.com>
2026-05-23 10:00:17 +0200

    Delete api/health.js

--- api/health.js
+++
@@ -1,6 +0,0 @@
-const cors = require("../lib/cors");
-module.exports = function handler(req, res) {
-  cors(res);
-  if (req.method === "OPTIONS") return res.status(200).end();
-  res.status(200).json({ ok: true, volt: "API v1.0" });
-};
